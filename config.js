1d91699368c6b0699b7209bead23d3b723ae4996
MykeJL <128696170+MykeJL@users.noreply.github.com>
2024-10-01 11:13:45 +0300

    - FB Deploy 2

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
-// config.js
 const firebaseConfig = {
-    apiKey: "AIzaSyCgB8i2HgfTy3I-Mwb7Wk7heGD2l83eZyc",
-    authDomain: "mdata-42fa6.firebaseapp.com",
-    projectId: "mdata-42fa6",
-    storageBucket: "mdata-42fa6.appspot.com",
-    messagingSenderId: "524100858912",
-    appId: "1:524100858912:web:55c20c4248533a5e387845"
-  };
+  apiKey: "AIzaSyCdj5IHb1afezFQBHJegFNRejKoscoQMF0",
+  authDomain: "mdata-42fa6.firebaseapp.com",
+  projectId: "mdata-42fa6",
+  storageBucket: "mdata-42fa6.appspot.com",
+  messagingSenderId: "524100858912",
+  appId: "1:524100858912:web:55c20c4248533a5e387845"
+};
   
   export default firebaseConfig;
